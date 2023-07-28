f8d68ce2f0885fa5aa33d1ea9d41adc1066b9ecf
Ethanall94 <Ethanall94@gmail.com>
2023-07-28 14:29:45 +0900

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,4 @@
 function toggleSidebar() {
     const sidebar = document.querySelector(".sidebar");
-    const containerList = document.querySelector(".container-list");
     sidebar.classList.toggle("active");
-    containerList.classList.toggle("container_big")
+}
