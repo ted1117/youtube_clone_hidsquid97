5cf9786d8a208823cf407ba3394bc29eb3c98f55
Ethanall94 <Ethanall94@gmail.com>
2023-08-03 17:48:48 +0900

    Update channelVideo.js

--- channelVideo.js
+++ channelVideo.js
@@ -15,22 +15,21 @@ function channelVideo(video_channel) {
             if (response !== undefined) {
 
                 // 데이터 확인
-                console.log(response[0].upload_date);
-                console.log(response[0].video_channel);
-                console.log(response[0].video_detail);
-                console.log(response[0].video_id);
-                console.log(response[0].video_tag);
-                console.log(response[0].video_title);
-                console.log(response[0].views);
+                // console.log(response[0].upload_date);
+                // console.log(response[0].video_channel);
+                // console.log(response[0].video_detail);
+                // console.log(response[0].video_id);
+                // console.log(response[0].video_tag);
+                // console.log(response[0].video_title);
+                // console.log(response[0].views);
 
                 for (i in response) {
-
                     const playlistVideo = document.querySelector(".playlistVideo");
 
-                    // // 썸네일 추가 코드, 썸네일 바로 추가 못 해요
-                    // const playThumbnail = document.createElement("img");
-                    // playThumbnail.setAttribute('src', response[i].);
-                    // playThumbnail.classList.add("playlistDesc");
+                    // // 썸네일 추가 코드, videoAPI에서 ID 받아서 처리해야할 듯
+                    const playThumbnail = document.createElement("img");
+                    playThumbnail.setAttribute('src', 'images/image 1.svg');
+                    playThumbnail.classList.add("playThumbnail");
 
                     const playlistDesc = document.createElement("div");
                     playlistDesc.classList.add("playlistDesc");
@@ -44,13 +43,16 @@ function channelVideo(video_channel) {
                     const plVideoChannel = document.createElement("span");
                     plVideoChannel.innerHTML = response[i].video_channel;
                     plVideoChannel.classList.add("plVideoInfo");
-                    plVideoChannel.classList.add("plVideoChannel");
+                    plVideoChannel.id = "plVideoChannel";
 
                     // 조회수, 업로드일
                     const plVideoViews = document.createElement("span");
                     plVideoViews.innerHTML += adjustUnit(response[i].views) +" "+ calcDateDiff(response[i].upload_date);
                     plVideoViews.classList.add("plVideoInfo");
-                    plVideoViews.classList.add("plVideoViews");
+                    plVideoViews.id = "plVideoViews";
+
+                    // 썸네일 테스트용 코드
+                    playlistVideo.appendChild(playThumbnail);
 
                     playlistDesc.appendChild(plVideoName);
                     playlistDesc.appendChild(plVideoChannel);
@@ -61,44 +63,8 @@ function channelVideo(video_channel) {
             }
         }
     };
-
     // 요청 전송
     xhr.send();
-}
-
-function adjustUnit(views) {
-    if (views > 10000) {
-        return (views / 10000).toFixed(1) + "M";
-    } else if (views > 1000) {
-        return (views / 1000).toFixed(1) + "K";
-    } else {
-        return views;
-    }
-}
-
-function calcDateDiff(date) {
-    const inputDate = new Date(date);
-    const currentDate = new Date();
-
-    // 두 날짜의 차이 (ms)
-    const msDiff = currentDate - inputDate;
-
-    // 두 날짜의 차이 (일)
-    const daysDiff = msDiff / (1000 * 60 * 60 * 24);
-
-    // 날짜 계산
-    if (daysDiff >= 365) {
-        const yearsDiff = Math.floor(daysDiff / 365);
-        return `${yearsDiff}년 전`;
-    } else if (daysDiff >= 30) {
-        const monthsDiff = Math.floor(daysDiff / 30);
-        return `${monthsDiff}달 전`;
-    } else if (daysDiff >= 7) {
-        const weeksDiff = Math.floor(daysDiff / 7);
-        return `${weeksDiff}주 전`;
-    } else {
-        return `${Math.floor(daysDiff)}일 전`;
-    }
 }
 
 channelVideo(video_channel);
