f5d282e686ce0f3a3376a478a7cb7d24d92d4670
ted111712 <ted111712@gmail.com>
2023-08-04 11:37:01 +0900

    Feat: 추천 영상 목록 기능 추가
    secondary에 영상을 추가하고 필터링 하는 기능 추가

--- video.js
+++ video.js
@@ -1,12 +1,13 @@
 let urlParams = new URLSearchParams(window.location.search);
 let id = urlParams.get('id');
 
+// 영상 정보 불러오기
 function createVideoItem(id) {
     // XMLHttpRequest 객체 생성
     let xhr = new XMLHttpRequest();
 
     // API 요청 설정
-    let apiUrl = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${id}`;
+    let apiUrl = `https://oreumi.appspot.com/video/getVideoInfo?video_id=${id}`;
     xhr.open("GET", apiUrl, true);
 
     // 응답 처리 설정
@@ -48,23 +49,19 @@ function createVideoItem(id) {
 
                 // 조회수 업로드
                 const videoViews = document.getElementById("videoInfoText");
-                videoViews.textContent = adjustUnit(response.views) + " " + calcDateDiff(response.upload_date);
+                videoViews.textContent = `조회수 ${adjustUnit(response.views)}회 ${calcDateDiff(response.upload_date)}`;
 
                 // 채널명
                 const channelName = document.getElementById("channelName");
                 channelTitle = response.video_channel;
                 channelName.textContent = channelTitle;
 
-                // 채널 이미지
-                let urlTitle = String(channelTitle).split(" ").join("_");
-                let imgUrl = `https://storage.googleapis.com/oreumi.appspot.com/${urlTitle}_profile.jpg`;
-                const channelImg = document.querySelector(".channelImg");
-                channelImg.src = imgUrl;
-
-                // 구독자 수
-                getSubs(channelTitle, function (subsCount) {
+                // 구독자 수 + 채널 프로필 이미지
+                getSubsPic(channelTitle, function (subsCount, channelPic) {
                     const subscribers = document.getElementById("subscribers");
+                    const channelImg = document.querySelector(".channelImg");
                     subscribers.textContent = `${subsCount} subscribers`;
+                    channelImg.src = channelPic;
                 });
 
                 // 영상 설명
@@ -82,12 +79,13 @@ function createVideoItem(id) {
     xhr.send();
 }
 
-function getSubs(video_channel, callback) {
+// 채널의 구독자수와 프로필 이미지 불러오기
+function getSubsPic(video_channel, callback) {
     // XMLHttpRequest 객체 생성
     let xhr = new XMLHttpRequest();
 
     // API 요청 설정
-    let apiUrl = `http://oreumi.appspot.com/channel/getChannelInfo?video_channel=${video_channel}`;
+    let apiUrl = `https://oreumi.appspot.com/channel/getChannelInfo?video_channel=${video_channel}`;
     xhr.open("POST", apiUrl, true);
 
     // 응답 처리 설정
@@ -97,9 +95,10 @@ function getSubs(video_channel, callback) {
             let response = JSON.parse(xhr.responseText);
 
             if (response && response.channel_name !== undefined) {
-                // 구독자 수
+                // 구독자 수 + 채널 프로필 이미지
                 let subsCount = adjustUnit(response.subscribers);
-                callback(subsCount);
+                let channelPic = response.channel_profile;
+                callback(subsCount, channelPic);
             }
         }
     };
@@ -108,4 +107,129 @@ function getSubs(video_channel, callback) {
     xhr.send();
 }
 
+// getVideoList API
+async function getVideoList() {
+    const apiURL = 'https://oreumi.appspot.com/video/getVideoList';
+
+    try {
+        const response = await fetch(apiURL);
+
+        // 응답이 성공적인지 확인합니다.
+        if (!response.ok) {
+            throw new Error('네트워크 응답이 올바르지 않습니다');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('데이터를 가져오는 중 오류 발생:', error);
+        return null;
+    }
+}
+
+// 추천 영상 목록 구성
+async function renderVideoList() {
+    const topMenu = document.querySelector(".top-menu");
+    try {
+        const data = await getVideoList();
+
+        for (i in data) {
+
+            if (id == i) {
+                // 현재 동영상은 추천 영상 목록에서 제외
+                continue;
+            }
+            // 영상 틀
+            const secVidList = document.createElement("a");
+            secVidList.classList.add("secondary-video-list");
+            secVidList.href = `./video.html?id=${data[i].video_id}`;
+            secVidList.style.textDecoration = "none";
+
+            // 영상 썸네일 틀
+            const smallThumbnail = document.createElement("a");
+            smallThumbnail.classList.add("small-thumbnail");
+
+            // 영상 썸네일 이미지
+            const thumbnailImg = document.createElement("img");
+            thumbnailImg.src = `https://storage.googleapis.com/oreumi.appspot.com/img_${data[i].video_id}.jpg`;
+
+            // 영상 정보 틀
+            const vidInfo = document.createElement("div");
+            vidInfo.classList.add("secondary-vid-info");
+
+            // 영상 제목
+            const vidTitle = document.createElement("a");
+            vidTitle.textContent = data[i].video_title;
+
+            // 채널 이름
+            const channelName = document.createElement("p");
+            channelName.textContent = `${data[i].video_channel}`;
+
+            // 조회수 + 업로드 일자
+            const vidViews = document.createElement("p");
+            vidViews.textContent = `조회수 ${adjustUnit(data[i].views)}회 · ${calcDateDiff(data[i].upload_date)}`;
+
+            vidInfo.appendChild(vidTitle);
+            vidInfo.appendChild(channelName);
+            vidInfo.appendChild(vidViews);
+
+            smallThumbnail.appendChild(thumbnailImg);
+
+            secVidList.append(smallThumbnail);
+            secVidList.appendChild(vidInfo);
+
+            topMenu.appendChild(secVidList);
+        }
+
+
+    } catch (error) {
+        console.error("비디오 목록을 가져오는 중 오류 발생: ", error);
+    }
+}
+
+// 현재 채널의 영상만 보이기
+function hideVideos(title) {
+    const secondaryVideoListContainers = document.querySelectorAll(".secondary-video-list");
+
+    secondaryVideoListContainers.forEach(container => {
+        const pElement = container.querySelector(".secondary-vid-info p:nth-child(2)");
+        const channelName = pElement.textContent.trim();
+
+        if (channelName !== title) {
+            container.style.display = "none";
+        }
+    });
+}
+
+// 다시 모든 영상 보이기
+function showAllVideos(title) {
+    const secondaryVideoListContainers = document.querySelectorAll(".secondary-video-list");
+
+    secondaryVideoListContainers.forEach(container => {
+        if (channelName !== title) {
+            container.style.display = "flex";
+        }
+    });
+}
+// 채널명 전역변수화
+let channelTitle;
+
+// 영상 재생
 createVideoItem(id);
+
+// 추천 영상 목록
+renderVideoList();
+
+// 추천 영상 목록에서 채널 별 필터링
+const allChannel = document.querySelector(".allChannel");
+const currnetChannel = document.querySelector(".channel");
+
+// allChannel 클릭 시 현재 영상을 제외한 모든 영상 보이기
+allChannel.addEventListener("click", function () {
+    showAllVideos();
+});
+
+// channel 클릭 시 현재 영상과 동일한 채널의 영상만 보이기
+currnetChannel.addEventListener("click", function () {
+    hideVideos(channelTitle);
+});
