d64c59b4f6fdcd217867032f405708e1d084f3be
ted111712 <ted111712@gmail.com>
2023-08-07 22:23:37 +0900

    Fix: index top-menu 너비 조정
    사이드바 확장/축소에 따라 top-menu의 너비도 바뀌도록 수정

--- hamburger.js
+++ hamburger.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const sidebar = document.querySelector(".sidebar");
 const section = document.querySelector("section");    // index.html에서 sidebar를 제외한 영역
 const main = document.querySelector("main");          // channel.html에서 sidebar를 제외한 영역
+const topMenu = document.querySelector(".top-menu");
 let open = true;    // 메뉴 확장 플래그
 
 hamburger.onclick = function toggleSidebar() {
@@ -12,6 +13,8 @@ hamburger.onclick = function toggleSidebar() {
             // index.html에서
             section.style.left = "240px";                  // section의 위치를 왼쪽에서 sidebar의 너비만큼 띄우기
             section.style.width = "calc(100% - 240px)";    // section의 너비를 전체에서 sidebar를 제외한 만큼으로 설정
+
+            topMenu.style.width = "calc(100% - 240px)";
         }
 
         if (main) {
@@ -24,6 +27,8 @@ hamburger.onclick = function toggleSidebar() {
         if (section) {
             section.style.left = "75px";
             section.style.width = "calc(100% - 75px)";
+
+            topMenu.style.width = "calc(100% - 75px)";
         }
 
         if (main) {
