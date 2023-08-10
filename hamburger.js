1bdcbef4253ff667a0df60d0c167aa3ee1a93935
ted111712 <ted111712@gmail.com>
2023-08-11 00:41:34 +0900

    Feat: 미니 사이드바 설정 추가
    open 플래그에 따라 미니 사이드바 visibility 속성 변경

--- hamburger.js
+++ hamburger.js
@@ -3,10 +3,11 @@ const sidebar = document.querySelector(".sidebar");
 const section = document.querySelector("section");    // index.html에서 sidebar를 제외한 영역
 const main = document.querySelector("main");          // channel.html에서 sidebar를 제외한 영역
 const topMenu = document.querySelector(".top-menu");
+const miniSidebar = document.querySelector(".miniSidebar");
 let open = true;    // 메뉴 확장 플래그
 
 hamburger.onclick = function toggleSidebar() {
-    sidebar.classList.toggle("active");
+    // sidebar.classList.toggle("active");
 
     if (!open) {
         if (section) {
@@ -22,19 +23,23 @@ hamburger.onclick = function toggleSidebar() {
             main.style.left = "240px";
             main.style.width = "calc(100% - 240px)";
         }
+        sidebar.style.display = "block";
+        miniSidebar.style.visibility = "hidden";
         open = true;
     } else {
         if (section) {
-            section.style.left = "75px";
-            section.style.width = "calc(100% - 75px)";
+            section.style.left = "72px";
+            section.style.width = "calc(100% - 72px)";
 
-            topMenu.style.width = "calc(100% - 75px)";
+            topMenu.style.width = "calc(100% - 72px)";
         }
 
         if (main) {
-            main.style.left = "75px";
-            main.style.width = "calc(100% - 75px)";
+            main.style.left = "72px";
+            main.style.width = "calc(100% - 72px)";
         }
+        sidebar.style.display = "none";
+        miniSidebar.style.visibility = "visible";
         open = false;
     }
 }
